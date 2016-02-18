dfcb5fd15b6e11e3fc7253631847adad89b24972
Ju/TroisYaourts <julien@troisyaourts.com>
2016-02-18 18:34:02 +0100

    sound loop

--- Player.py
+++ Player.py
@@ -3,6 +3,11 @@ import pygame
 import sys
 import hashlib
 import os
+import time
+from time import sleep
+import threading
+from threading import Thread
+import queue
 
 def shellquote(s):
     return "'" + s.replace("'", "'\\''") + "'"
@@ -15,6 +20,19 @@ class Player:
       pygame.init()
       pygame.mixer.init()
 
+    self.running = threading.Event()
+    self.queue = queue.Queue()
+    self.loop = PlayerLoop(self.running, queue=self.queue)
+    self.start()
+
+  def start(self):
+    self.running.set()
+    self.loop.start()
+
+  def clean(self):
+    self.running.clear()
+    self.loop.join()
+
 
   def say(self, text, voice="Thomas"):
     filename = "assets/"+hashlib.md5(text.encode('utf-8')).hexdigest()
@@ -25,7 +43,7 @@ class Player:
       #os.system("say -v "+voice+ " -o "+soundfile+" --data-format=LEF32@22050 "+esc_desc+" && ffmpeg -i "+soundfile+"  -acodec libvorbis "+oggfile+"")
     else:
       sound= pygame.mixer.Sound(filename+".ogg")
-      sound.play()
+      self.queue.add(sound)
 
   def preload(self, text, voice="Thomas"):
     filename = "assets/"+hashlib.md5(text.encode('utf-8')).hexdigest()
@@ -33,5 +51,29 @@ class Player:
       esc_desc = shellquote(text)
       os.system("say -v "+voice+ " -o "+filename+".wav --data-format=LEF32@22050 "+esc_desc+" && ffmpeg -i "+filename+".wav  -acodec libvorbis "+filename+".ogg")
 
+class PlayerLoop(Thread):
+  def __init__(self,event, queue = None):
+    Thread.__init__(self)
+    self.daemon = True
+    self.runningEvent = event
+    self.queue = queue
+
+  def run(self):
+    initial = time.time()
+    while self.runningEvent.is_set():
+      try:
+        if self.queue is not None:
+          sound = self.queue.get(False)
+          sound.play()
+          while sound.get_busy()
+            continue
+          continue
+      except queue.Empty:
+        continue
+      except Exception as e:
+        continue
+      sleep(.1)
+    print("Player Subroutine ended by clearing runningEvent")
+    self.clean()
 
     
